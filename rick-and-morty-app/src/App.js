1a69e39f896b5e922d43604b56cf099ee6d68bb5
guibad <guillem.badenas@dxc.com>
2023-03-22 09:46:18 +0100

    Creado nuevo repo

---
+++ rick-and-morty-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Navbar } from "./components/Navbar";
+import { Characters } from './components/Characters';
+import { Pagination } from './components/Pagination';
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+  const initialURL = "https://rickandmortyapi.com/api/character"
+
+  const fetchCharacters = (initialURL) => {
+    fetch(initialURL)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch(error => console.log(error))
+  }
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialURL);
+  }, [])
+
+  return (
+    <>
+      <Navbar brand={"Rick and Morty App"} />
+      <div className='container mt-5'>
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+        <Characters characters={characters} />
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
---
+++ rick-and-morty-app/src/components/Characters.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const Characters = ({ characters = [] }) => {
+  return (
+    <div className='row'>
+      {
+        characters.map((item, index) => (
+          <div key={index} className='col mb-4'>
+            <div className='card' style={{minWidth: "200px", minHeight:"500px"}}>
+              <img src={item.image} alt='Imagen'></img>
+              <div className='card-body'>
+                <h5 className='card-title'>{item.name}</h5>
+                <hr/>
+                <p>Location: {item.location.name}</p>
+                <p>Species: {item.species}</p>
+              </div>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
